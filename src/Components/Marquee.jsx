11797169e993fdf9f16a4a2edbd126203c7da54c
harshrao26 <harshurao058@gmail.com>
2024-08-06 16:49:12 +0530

    image

--- src/Components/Marquee.jsx
+++ src/Components/Marquee.jsx
@@ -1,30 +1,34 @@
 import { cn } from "../lib/utils.js";
 import Marquee from "./magicui/marquee";
+import simrat from '../../public/review/simrat.png'
+import jovial from '../../public/review/jovial.png'
+import daljeet from '../../public/review/daljeet.png'
+import ashish from '../../public/review/ashish.png'
 
 const reviews = [
   {
     name: "Simrat Ghai",
     username: "@simrat_ghai",
     body: "Awesome drink 🥤",
-    img: "https://instagram.fknu1-6.fna.fbcdn.net/v/t51.2885-19/163295620_350502036371162_4357479009439974334_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fknu1-6.fna.fbcdn.net&_nc_cat=102&_nc_ohc=d1gYGVHh2DcQ7kNvgEH0Fa-&edm=APHcPcMBAAAA&ccb=7-5&oh=00_AYCte8MtsqD7uFB9aySukCG7RurSShxX2RSfSdUfFK3-SA&oe=66B7CD71&_nc_sid=bef7bc",
+    img: {simrat},
   },
   {
     name: "Jovial Mudsssir",
     username: "@jovial_mudassir",
     body: "Too Yummy To Taste It..... Best Ever Drink To Taste It...",
-    img: "https://instagram.fknu1-5.fna.fbcdn.net/v/t51.2885-19/429181962_1457957451739661_5956242276024429398_n.jpg?_nc_ht=instagram.fknu1-5.fna.fbcdn.net&_nc_cat=105&_nc_ohc=7qOJ3gO-HFQQ7kNvgGqj-O3&edm=AEhyXUkBAAAA&ccb=7-5&oh=00_AYBFMQPxIj60B3ZlU6cPwBhW0KaikhOeBU2Z0LAsMih9eA&oe=66B7D5CC&_nc_sid=8f1549",
+    img: {jovial},
   },
   {
     name: "Daljeet Singh",
     username: "@daljeetsingh.singh.1969",
     body: "Nice taste , groovy mixed fruit",
-    img: "https://instagram.fknu1-5.fna.fbcdn.net/v/t51.2885-19/320749527_712692230205017_4896838675036726114_n.jpg?_nc_ht=instagram.fknu1-5.fna.fbcdn.net&_nc_cat=105&_nc_ohc=zr5EoIHqoEIQ7kNvgGN2WcT&edm=APHcPcMBAAAA&ccb=7-5&oh=00_AYD5U-4eNkC8dP6eL9LoZdSPD82KSjWuQ2b0mBKgyw0-vg&oe=66B7C3EE&_nc_sid=bef7bc",
+    img: {daljeet},
   },
   {
-    name: "Jane",
+    name: "Ashish Kumar",
     username: "@ashishkumarguptasai",
     body: "Jabardast test hai hamne piya hai",
-    img: "https://instagram.fknu1-1.fna.fbcdn.net/v/t51.2885-19/370191224_331754575950829_3528206785226424970_n.jpg?_nc_ht=instagram.fknu1-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=_8HcGs5p2EkQ7kNvgHGAg_m&edm=AEhyXUkBAAAA&ccb=7-5&oh=00_AYBi63fEMn7TDPNAAiFFZIvkGl_bE86fnWoxXA3PXA_8Bg&oe=66B7ED28&_nc_sid=8f1549",
+    img: {ashish},
   },
   {
     name: "Jenny",
@@ -105,3 +109,4 @@ export default function MarqueeDemo() {
     </div>
   );
 }
+0
