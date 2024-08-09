ad995cf60bc13a63daf98b0dabdc65a4efdf427f
harshrao26 <harshurao058@gmail.com>
2024-08-09 14:25:48 +0530

    Product card updated

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -13,15 +13,15 @@ const Gallery = () => {
 
   return (
     <div className="pt-16">
-      <div className="flex px-16 flex-col items-center justify">
-          <h2 className="mt-8 text-4xl items-center justify-center  leading-tight w-full text-black sm:text-4xl lg:text-4xl flex flex-col mb-4">
+      <div className="flex px-16 flex-col items-center justify ">
+          <h2 className="mt-8 text-4xl items-center justify-center leading-tight w-full text-black sm:text-4xl lg:text-4xl flex flex-col mb-4 ">
             <p className="font-semibold text-4xl">Our Distributers </p>
           </h2>
-          <div className="flex flex-wrap gap-4 rounded">
+          <div className="flex flex-wrap justify-between items-stretch rounded hover:scale-15">
             {dist.map((val, key) => {
               return (
-                <div className="rounded-xl">
-                  <img src={val} alt="" className="w-80 " />
+                <div className="rounded-xl ">
+                  <img src={val} alt="" className="w-96  " />
                 </div>
               );
             })}
